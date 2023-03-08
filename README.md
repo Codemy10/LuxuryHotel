a4ace95d1347ecb361a8518b80e6bbebfa20f37a
AbdulRahmon <108176525+Codemy10@users.noreply.github.com>
2023-03-08 10:50:07 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# LUXURY HOTEL
+Luxury Hotel is an improvement on hotel-bt my first repository.
+Luxury hotel is designed with an highly fashionable imagination.
+Luxury hotel is a simple well designed single paged website ,with high quality images to suit your eyes.
---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('body a').on('click ',function(event) {
+    if(this.hash !=='') {
+        event.preventDefault();
+
+        const hash= this.hash;
+        $('html,body').animate(
+            {
+                scrollTop:$(hash).offset().top - 50
+            },
+            1500
+        );
+    }
+});
